41bf219e46ed1c33ed4d4d5e1c5ac591929f13e4
Nathan Harris <nathanharris91@gmail.com>
2023-07-16 22:43:42 -0600

    initial commit

---
+++ 07-HashTables/HT-ItemInCommon2.py
@@ -0,0 +1,25 @@
+def item_in_common(list1, list2):
+    my_dict = {}
+    for i in list1:
+        my_dict[i] = True
+
+    for j in list2:
+        if j in my_dict:
+            return True
+
+    return False
+
+
+list1 = [1,3,5]
+list2 = [2,4,5]
+
+
+print(item_in_common(list1, list2))
+
+def item_in_common(list1, list2):
+    
+    for j in list2:
+        if j in list1:
+            return True
+
+    return False
